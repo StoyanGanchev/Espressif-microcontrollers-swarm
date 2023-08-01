73587b31427d7b645d19e8c65fb254fe67a3435e
Stoyan Ganchev <94966266+StoyanGanchev@users.noreply.github.com>
2023-08-01 16:56:04 +0300

    Delete Library test directory

--- Library test/lib/lib_test/lib_test.cpp
+++
@@ -1,6 +0,0 @@
-#include "lib_test.h"
-
-int addTwoInts(int a, int b)
-{
-  return a + b;
-}
--- Library test/lib/lib_test/lib_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIB_TEST_H
-#define LIB_TEST_H
-
-#include <Arduino.h>
-
-int addTwoInts(int a, int b);
-
-#endif
